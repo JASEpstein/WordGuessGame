89bb081e0acce40ab7dad5bec41049fb3474e6d8
JASEpstein <jasepstein@gmail.com>
2019-02-12 09:49:36 -0600

    Core game mechanic works! Needs conditionals and counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,124 +1,121 @@
+/*
+PSEUDOCODE
+
+1.) After user loads page, press any key to start game. x
+2.) When key is pressed, the 'Press Any Key' text disappears. x
+3.) A string of hashes appears. x
+4.) Guessing the correct letters will reveal the corresponding letter in the hash string.
+5.) Guessing the incorrect letters will decrement a 'Mistakes' counter.
+6.) Previously guessed letters will appear below that counter. 
+7.) When the number of mistakes reaches 0, the game resets.
+
+*/
 // Global Variables
 
-var alphabetArray = [];
-var keyMatch = "";
 var winsCount = 0;
 var lossesCount = 0;
 var guessesSoFar = [];
 var guessesLeftCount = 10 - guessesSoFar.length;
 var wordToGuess = '';
 var startUp = false;
-var start = '';
 var randomMovie = '';
+var wordHash = '';
+var movie = '';
 
-//Generates alphabet array
-function genCharArray(charA, charZ) {
-    alphabetArray = [],
-        i = charA.charCodeAt(0),
-        j = charZ.charCodeAt(0);
-    for (; i <= j; ++i) {
-        alphabetArray.push(String.fromCharCode(i));
-    }
-    alphabetArray.push(' ');
-    return alphabetArray;
-}
-genCharArray('a', 'z');
-
-//Movie Database Object
-var movieDatabase = [
-    TotalRecall = {
+//Movie Array
+var movies = [
+    {
         name: "Total Recall",
         url: "url('https://goo.gl/images/2uzU31')"
     },
-    SayAnything = {
+    {
         name: "Say Anything",
         url: "url('https://goo.gl/images/mgv9uf')"
     },
-    BackToTheFuture = {
+    {
         name: "Back To The Future",
         url: "url('https://goo.gl/images/x1zGar')"
     },
-    Highlander = {
+    {
         name: "Highlander",
         url: "url('https://goo.gl/images/w7WxZc')"
     },
-    TopGun = {
+    {
         name: "Top Gun", 
         url: "url('https://goo.gl/images/hBgf4J')"
     },
-    Poltergeist = {
+    {
         name: "Poltergeist", 
         url: "url('https://goo.gl/images/qBfzox')"
     },
-    TheShining = {
+    {
         name: "The Shining",
         url: "url('https://goo.gl/images/t4xNrR')"
     },
-    TheGoonies = {
+    {
         name: "The Goonies", 
         url: "url('https://goo.gl/images/MDH2ci')"
-    },
-    Ghostbusters = {
+    },{
         name: "Ghostbusters",
         url: "url('https://goo.gl/images/Zhnstg')"
     },
-    TheTerminator = {
+    {
         name: "The Terminator",
         url: "url('https://goo.gl/images/75zPwD')"
-    },
+    }
 ];
 
+
+function updateHash() {
+    document.getElementById("wordHash").textContent = wordHash;
+}
 //Resets the game
 function roundReset() {
-    guessesSoFar = [];
-    document.getElementById("guessesSoFar").textContent = guessesSoFar;
-    document.getElementById("guessesLeft").textContent = 10 - guessesSoFar.length;
+  
 }
 
-function setUpWordToGuess() {
-    var randomArrayItem = movieDatabase[Math.floor(Math.random() * movieDatabase.length)];
+function makeAGuess(guessLetter){
+    //Set an empty array called letterPositions[]
+    movie = wordToGuess.split('');
+    hash = wordHash.split('');
+    //Look through wordToGuess for instances of guessLetter
+    movie.forEach(function (character, index) {
+        if (character === guessLetter){
+            hash[index] = guessLetter;
+        }
+    });
+    //If one or more instances of guessLetter exist in wordToGuess, record position in letterPositions[]
+    //For each letter position in letterPositions[], replace the corresponding hash in wordHash with guessLetter 
+    //If no instances of guessLetter exist in wordToGuess, iterate the Mistakes counter
+    //If fail condition is met, call roundReset()
+    wordHash = hash.join("");
+    updateHash();
+}
+
+function roundStart(){
+    //1st Pick a random movie from array
+    var randomArrayItem = movies[Math.floor(Math.random() * movies.length)];
     wordToGuess = randomArrayItem.name;
-};
+    //2nd Create identical length string of hashes
+    for (i = 0; i < wordToGuess.length; i++) {
+        wordHash += "#";
+    }
+    //Initial display wordHash in the browser
+    updateHash();
 
-function setUpGuessBlocks() {
-
+    //Update User instructions
+    document.getElementById('instructions').textContent = 'Start Guessing Letters!';
 }
+
 
 //Startup on 'Press Any Key'
-document.onkeyup = function(start){
-    start = alphabetArray.find(function (){
-        start.key === ' ';
-    });
-    startUp = true;
-   setUpWordToGuess();
-};
-
-
-
-//Keyups for Guessing
-document.onkeyup = function (e) {
-    keyMatch = alphabetArray.find(function (letterMatch) {
-        return letterMatch === e.key;
-    });
-    //Win Criteria
+document.onkeyup = function(e){
     if (startUp === true){
-        if (keyMatch === randomLetter) {
-            document.getElementById("wins").textContent = ++winsCount;
-            roundReset();
-            //Exclude keys that aren't the alphabet
-        } else if (keyMatch === undefined) {
-            return;
-            //Loss Criteria
-        } else {
-            guessesSoFar.push(keyMatch);
-            document.getElementById("guessesLeft").textContent = 10 - guessesSoFar.length;
-            document.getElementById("guessesSoFar").textContent = guessesSoFar;
-        }
-        //Loss Counter to Reset after too many guesses
-        if (guessesSoFar.length === 10) {
-            document.getElementById("losses").textContent = ++lossesCount;
-            roundReset();
-        }
+        //Checks if game is started, proceeds to execute the game
+        makeAGuess(e.key);
+    } else {
+        //Allows the game to start on next key press
+        startUp = true;
+        roundStart();
     }
-       
-   };
+};
